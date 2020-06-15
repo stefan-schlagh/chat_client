8bed57f9bb1bd78f6763a084a38af69059f00a24
stefan-schlagh <stefan.schlaghuber@gmail.com>
2020-06-15 14:34:42 +0200

    new normalChat as http-request

--- src/chatData/tempChatLoader.js
+++ src/chatData/tempChatLoader.js
@@ -44,56 +44,66 @@ export default class TempChatLoader{
      */
     async createNewNormalChat(message){
 
-        return new Promise((resolve,reject) => {
-            /*
-                the request gets sent to the server
-             */
-            const otherUid = this.chatNow.otherUser;
-            const otherUsername = this.chatNow.chatName;
+        /*
+            the request gets sent to the server
+         */
+        const otherUid = this.chatNow.otherUser;
+        const otherUsername = this.chatNow.chatName;
 
-            /*
-                TODO: http-request
-             */
-            chatSocket.socket.emit('new normalChat',{
+        const config = {
+            method: 'PUT',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
                 uid: otherUid,
                 username: otherUsername,
                 message: message
-            },res => {
-                /*
-                    the user and the chat get created client-side
-                 */
-                if(chatSocket.users.getIndex(otherUid) === -1){
-                    chatSocket.users.add(otherUid,new User(otherUid,otherUsername,res.online));
-                }
-                const otherUser = chatSocket.users.get(otherUid);
-                otherUser.online = res.online;
+            })
+        };
 
-                const newChat = new NormalChat(res.ncid,otherUsername,otherUid);
-                /*
-                    chat gets added to user
-                 */
-                otherUser.normalChat = newChat.id;
-                /*
-                    chat is added in binsearchArray
-                 */
-                chatSocket.chats.normal.add(res.ncid,newChat);
-                /*
-                    message is added to chat
-                 */
-                newChat.messages.add(res.mid,new Message(res.mid,message,chatSocket.userSelf.uid,newChat,new Date(Date.now())));
+        const response = await fetch('/user/chat', config);
 
-                //args: chat
-                getDispatch().addChat(newChat);
+        if(response.ok){
 
-                /*
-                    currentChat gets changed
-                 */
-                getDispatch().selectChat(newChat);
-                this.hide();
+            const data = await response.json();
 
-                resolve(true);
-            });
-        });
+            if(chatSocket.users.getIndex(otherUid) === -1){
+                chatSocket.users.add(otherUid,new User(otherUid,otherUsername));
+            }
+            const otherUser = chatSocket.users.get(otherUid);
+
+            const newChat = new NormalChat(data.ncid,otherUsername,otherUid);
+            /*
+                chat gets added to user
+             */
+            otherUser.normalChat = newChat.id;
+            /*
+                chat is added in binsearchArray
+             */
+            chatSocket.chats.normal.add(data.ncid,newChat);
+            /*
+                message is added to chat
+             */
+            newChat.messages.add(data.mid,new Message(data.mid,message,chatSocket.userSelf.uid,newChat,new Date(Date.now())));
+
+            //args: chat
+            getDispatch().addChat(newChat);
+            /*
+                currentChat gets changed
+             */
+            getDispatch().selectChat(newChat);
+            /*
+                tempChat is hidden
+             */
+            this.hide();
+
+            return true;
+        }
+
+        return false;
+
     }
 
     get isShown() {
