ecb52a2ebf43935f3e855e43c4ba673bd889b977
stefan-schlagh <stefan.schlaghuber@gmail.com>
2020-07-01 15:30:23 +0200

    better statusMessages

--- src/Home/chatView/chatContainer/message/StatusMessage.js
+++ src/Home/chatView/chatContainer/message/StatusMessage.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Dummy from "../../../../utilComp/Dummy";
-import {UsernameSpan} from "./Message";
-import {statusMessages} from "../../../../chatData/message/statusMessage";
+import {getStatusMessageString} from "../../../../chatData/message/statusMessage";
 
 export default function StatusMessage(props){
 
@@ -24,34 +23,10 @@ export default function StatusMessage(props){
 function StatusMsgContent(props) {
 
     const msg = props.msg;
-    /*
-        Du hast
-        userxy hat
-     */
-    const renderStatusMsg = (
-        otherUsers,
-        s2 = ""
-    ) => {
-        const passiveUsers = msg.content.passiveUsers;
-        return(
-            <span>
-                <UsernameSpan user={msg.userTop}/>
-                {msg.bySelf ? " hast " : " hat "}
-                {passiveUsers.length > 0
-                    ? (passiveUsers.length + " Benutzer ")
-                    : ""
-                }
-                {s2}
-            </span>
-        )
-    };
 
     return(
         <Dummy>
-            {renderStatusMsg(
-                msg.content.passiveUsers,
-                statusMessages[msg.content.type]
-            )}
+            {getStatusMessageString(msg)}
         </Dummy>
     );
 }
