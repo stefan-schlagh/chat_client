efc17a01b40f786c6a950781dfb644e158ecafea
stefan-schlagh <64643082+stefan-schlagh@users.noreply.github.com>
2021-01-24 12:10:40 +0100

    remove commented code in userInfoApiCalls

--- src/Home/userInfo/__mocks__/userInfoApiCalls.js
+++ src/Home/userInfo/__mocks__/userInfoApiCalls.js
@@ -1,35 +1,3 @@
-/*let called = 0;
-
-export const fetchUserInfo = () => {
-
-    called ++;
-
-    switch (called){
-        case 1:
-            return {
-                uidSelf: 2,
-                username: "test123",
-                blocked: false,
-                userExists: true
-            };
-        case 2:
-            throw new Error('Error fetching userInfo');
-        case 3:
-            return {
-                uidSelf: 2,
-                userExists: false
-            }
-        case 4:
-            return {
-                uidSelf: 2,
-                username: "test321",
-                blocked: true,
-                userExists: true
-            }
-        default:
-            return {}
-    }
-*/
 export const fetchUserInfo = jest.fn(() => ({
             uidSelf: 2,
             username: "test123",
